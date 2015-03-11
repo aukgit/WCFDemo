82357a0cf67919a6727e86b8bb0b192d6e197d43
Alim Ul Karim <devorg.bd@gmail.com>
2015-03-12 01:06:25 +0600

    failed

--- WCFMetaDemo/Client/Program.cs
+++ WCFMetaDemo/Client/Program.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Client.EmployeeService;
+using Client.ServiceReference1;
 
 namespace Client {
     class Program {
