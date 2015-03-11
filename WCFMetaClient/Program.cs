bdc37fcff4936b510cf8691fb2fc6b08430aa72b
Alim Ul Karim <devorg.bd@gmail.com>
2015-03-12 01:14:04 +0600

    Simple meta exchange working

---
+++ WCFMetaClient/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WCFMetaClient.ServiceReference1;
+
+namespace WCFMetaClient {
+    class Program {
+        static void Main(string[] args) {
+            Service1Client proxyClient = new Service1Client();
+            Console.WriteLine(proxyClient.GetMessage());
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ WCFMetaServer/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WCFMetaServer {
+    class Program {
+        static void Main(string[] args) {
+            using (ServiceHost host = new ServiceHost(typeof(Service1))) {
+                host.Open();
+                Console.WriteLine("Server is running at :");
+                if (host.Description.Endpoints != null) {
+                    var endPoints = host.Description.Endpoints;
+                    foreach (var endPoint in endPoints) {
+                        Console.WriteLine(endPoint.Address);
+                    }
+                }
+                Console.WriteLine("Press Enter to close.");
+                Console.ReadLine();
+            }
+        }
+    }
+}
