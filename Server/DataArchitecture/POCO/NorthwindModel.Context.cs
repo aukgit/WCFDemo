96a5766fe7bffc7b96540187c00efe65928d3acf
Alim Ul Karim <devorg.bd@gmail.com>
2015-03-12 00:37:35 +0600

    only keeeping employees

--- Server/DataArchitecture/POCO/NorthwindModel.Context.cs
+++ Server/DataArchitecture/POCO/NorthwindModel.Context.cs
@@ -16,8 +16,8 @@ namespace Server.DataArchitecture
     public partial class NorthwindEntities : DbContext
     {
         public NorthwindEntities()
-            : base("name=NorthwindEntities")
-        {
+            : base("name=NorthwindEntities") {
+            this.Configuration.LazyLoadingEnabled = false;
         }
     
         protected override void OnModelCreating(DbModelBuilder modelBuilder)
@@ -25,16 +25,8 @@ namespace Server.DataArchitecture
             throw new UnintentionalCodeFirstException();
         }
     
-        public virtual DbSet<Category> Categories { get; set; }
-        public virtual DbSet<Customer> Customers { get; set; }
-        public virtual DbSet<CustomerDemographic> CustomerDemographics { get; set; }
+     
         public virtual DbSet<Employee> Employees { get; set; }
-        public virtual DbSet<OrderDetail> OrderDetails { get; set; }
-        public virtual DbSet<ProductOrder> ProductOrders { get; set; }
-        public virtual DbSet<Product> Products { get; set; }
-        public virtual DbSet<Region> Regions { get; set; }
-        public virtual DbSet<Shipper> Shippers { get; set; }
-        public virtual DbSet<Supplier> Suppliers { get; set; }
-        public virtual DbSet<Territory> Territories { get; set; }
+      
     }
 }
